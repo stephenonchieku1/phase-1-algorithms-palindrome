eeb66975d9d248e9ac02de8172c7b2985b8aa2ca
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-27 14:35:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function isPalindrome(word) {
+//function isPalindrome(word) {
   // Write your algorithm here
+  
+
+function isPalindrome(word) {
+
+    var len = word.length;
+    var mid = Math.floor(len/2);
+
+    for ( var i = 0; i < mid; i++ ) {
+        if (word[i] !== word[len - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
+isPalindrome("eye")
+  
+
+
 
 /* 
   Add your pseudocode here
